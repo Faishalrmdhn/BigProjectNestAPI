f13da6c01d5513a12c84ea41686d816f177ee07c
Faishalrmdhn <faishalramadhan25@gmail.com>
2022-11-03 10:46:30 +0700

    set api testing for token and booking

--- tests/page/reqres.api.js
+++ tests/page/reqres.api.js
@@ -1,7 +1,10 @@
 import BaseAPI from "$root/page/base.api";
 
 const reqresApi = {
-    auth: (data) => BaseAPI.post('/auth', data)
+    auth: (data) => BaseAPI.post('/auth/', data),
+    getBookingId:(firstname, lastname) => BaseAPI.get(`/booking${firstname && lastname ? `?firstname=${firstname}&lastname=${lastname}`: ''}`),
+    getBooking: (id) => BaseAPI.get(`/booking/${id}`),
+    createBooking: (data)=> BaseAPI.post('/booking', data)
 }
 
 export default reqresApi;
